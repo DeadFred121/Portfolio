88ab59d77f71a2acf8d4aa33e4c7989dbc04612e
DeadFred <f.cliff1@hotmail.com>
2017-09-11 09:46:10 +1000

    updated meta tags

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,19 @@
 class WelcomeController < ApplicationController
   def index
+    @page_title       = 'Dead Fred Creative'
+    @page_description = 'Landing/Title Page'
+    @page_keywords    = 'Creative, Homepage'
   end
 
   def contact
+    @page_title       = 'Contact'
+    @page_description = 'Contact Details'
+    @page_keywords    = 'Contact, Links, Information'
   end
 
   def about
+    @page_title       = 'About'
+    @page_description = 'Description of Author'
+    @page_keywords    = 'Description, Author, Details'
   end
 end
